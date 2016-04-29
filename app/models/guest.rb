717eedb716e33ccbadd693ea0907f8741638250b
Christine Onayemi <caonayemi@gmail.com>
2016-04-29 15:46:23 -0500

    Add address method to Guest

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,18 @@ class Guest < ActiveRecord::Base
   has_many :rsvps
   has_many :invitations, through: :rsvps
   has_many :events, through: :invitations
+
+  # Add validations to make sure that:
+  # 1) No guest is both head of household and partner
+  # 2) No guest is both child and head of household
+  # 3) No guest is both child and partner
+  # 4) Age group is one of the following: adult, teen, child, infant
+
+  def address
+    if address_line_2
+      address_line_1 + "\n" + address_line_2 + "\n" + city + ", " + state + " " + zip + "\n" + country
+    else
+      address_line_1 + "\n" + city + ", " + state + " " + zip + "\n" + country
+    end
+  end
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+RSpec.describe Guest, ".address" do
+  before(:each) do
+    @guest = Guest.create!(
+      title: "Mr.",
+      first_name: "Adekunle",
+      last_name: "Onayemi",
+      priority: "A",
+      is_head_of_household: true,
+      is_partner: nil,
+      age_group: "adult",
+      address_line_1: "706 North Taylor Avenue",
+      address_line_2: nil,
+      city: "Oak Park",
+      state: "Illinois",
+      zip: "60302"
+    )
+  end
+
+  it "should return a string" do
+    expect( @guest.address ).to be_a( String )
+  end
+
+  it "should return at least three lines of text" do
+    expect( /.+\n.+\n.+/.match(@guest.address) ).not_to be_nil
+  end
+
+  it "should return only three lines of text if no 'line 2' is provided" do
+    expect( /.+\n.+\n.+\n.+/.match(@guest.address) ).to be_nil
+  end
+
+  it "should return four lines of text if there is a 'line 2'" do
+    @guest.update_attributes( address_line_2: "No. 7" )
+    expect( /.+\n.+\n.+\n.+/.match(@guest.address) ).not_to be_nil
+  end
 end
