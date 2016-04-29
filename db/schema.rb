11acf171560d414135147613af1949af5d665a80
Christine Onayemi <caonayemi@gmail.com>
2016-04-29 15:15:47 -0500

    Add Rspec files for models

---
+++ db/schema.rb
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160429200619) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.date     "to_be_held_on"
+    t.date     "send_invitations_by"
+    t.date     "rsvps_due_on"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.string   "title"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "priority"
+    t.boolean  "is_head_of_household"
+    t.boolean  "is_partner"
+    t.string   "age_group"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "country",              default: "United States"
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer  "event_id"
+    t.string   "titles"
+    t.string   "first_names"
+    t.string   "family_name"
+    t.string   "additional_guests"
+    t.date     "sent_on"
+    t.string   "gifts"
+    t.date     "thank_you_sent_on"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.integer  "guest_id"
+    t.integer  "invitation_id"
+    t.date     "received_on"
+    t.boolean  "is_attending"
+    t.string   "meal_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
